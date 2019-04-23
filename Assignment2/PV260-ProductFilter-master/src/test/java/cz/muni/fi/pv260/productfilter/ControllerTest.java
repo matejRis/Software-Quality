90722963bcfd861586ea7a79e649592db3ce8cb2
xmokros <456442@mail.muni.cz>
2019-04-23 22:56:06 +0200

    finished task 04

--- Assignment2/PV260-ProductFilter-master/src/test/java/cz/muni/fi/pv260/productfilter/ControllerTest.java
+++ Assignment2/PV260-ProductFilter-master/src/test/java/cz/muni/fi/pv260/productfilter/ControllerTest.java
@@ -3,12 +3,12 @@ package cz.muni.fi.pv260.productfilter;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.stubbing.Answer;
 
 import java.util.Arrays;
 import java.util.Collection;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
 public class ControllerTest {
@@ -41,5 +41,34 @@ public class ControllerTest {
         controller.select(filter);
         verify(output).postSelectedProducts(argument.capture());
         assertEquals(Arrays.asList(productBlack), argument.getValue());
+    }
+
+    @Test
+    public void testControllerLogsMessageOnSuccess() throws ObtainFailedException {
+        when(input.obtainProducts()).thenReturn(Arrays.asList(productBlack, productWhite));
+        ArgumentCaptor<String> argumentTag = ArgumentCaptor.forClass((String.class));
+        ArgumentCaptor<String> argumentString = ArgumentCaptor.forClass((String.class));
+        controller.select(filter);
+        verify(logger).log(argumentTag.capture(), argumentString.capture());
+        assertTrue(Controller.class.getSimpleName().equals(argumentTag.getValue()) &&
+                "Successfully selected 1 out of 2 available products.".equals(argumentString.getValue()));
+    }
+
+    @Test
+    public void testExceptionOccursWhenObtainingProductData() throws ObtainFailedException {
+        when(input.obtainProducts()).thenThrow(ObtainFailedException.class);
+        ArgumentCaptor<String> argumentTag = ArgumentCaptor.forClass((String.class));
+        ArgumentCaptor<String> argumentString = ArgumentCaptor.forClass((String.class));
+        controller.select(filter);
+        verify(logger).log(argumentTag.capture(), argumentString.capture());
+        assertTrue(Controller.class.getSimpleName().equals(argumentTag.getValue()) &&
+                ("Filter procedure failed with exception: " + ObtainFailedException.class.getName()).equals(argumentString.getValue()));
+    }
+
+    @Test
+    public void testExceptionOccursWhenObtainingProductDataAndNothingIsInOutput() throws ObtainFailedException {
+        when(input.obtainProducts()).thenThrow(ObtainFailedException.class);
+        controller.select(filter);
+        verify(output, never()).postSelectedProducts(Arrays.asList(productBlack));
     }
 }
